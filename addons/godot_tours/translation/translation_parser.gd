957558d11ff9c59c4b08a48572eaf13ed91833d6
Răzvan C. Rădulescu <razcore.rad@gmail.com>
2024-04-22 17:36:32 +0300

    fix: extract POT string correctly when escaping & trailing with `\`
    fixes #43
    fixes #45
    closes #46 because it superceeds it

--- addons/godot_tours/translation/translation_parser.gd
+++ addons/godot_tours/translation/translation_parser.gd
@@ -2,8 +2,8 @@
 extends EditorTranslationParserPlugin
 
 const PATTERNS := [
-	"""gtr\\(\\s*['"]+(.*?)['"]+\\s*(?:,\\s*['"]+(.*?)['"]+\\s*)?\\)""",
-	"""gtr_n\\(\\s*['"]+(.*?)['"]+\\s*,\\s*['"]+(.*?)['"]+\\s*,\\s*(?:\\d+)\\s*(?:,\\s*['"]+(.*?)['"]+\\s*)?\\)""",
+	r"""gtr\(\s*['"]+(.*?(?:\\")?)['"]+\s*(?:,\s*['"]+(.*?)['"]+\s*)?\)""",
+	r"""gtr_n\(\s*['"]+(.*?(?:\\")?)['"]+\s*,\s*['"]+(.*?(?:\\")?)['"]+\s*,\s*(?:\d+)\s*(?:,\s*['"]+(.*?)['"]+\s*)?\)""",
 ]
 
 var regex := RegEx.new()
@@ -14,14 +14,18 @@ func _parse_file(path: String, msgids: Array[String], msgids_context_plural: Arr
 	for pattern in PATTERNS:
 		regex.compile(pattern)
 		for regex_match in regex.search_all(source_code):
-			var singular := regex_match.strings[1]
+			var singular := _replace_quotes(regex_match.strings[1])
 			var context := regex_match.strings[2]
 			var plural := ""
 			if regex_match.get_group_count() > 2:
-				plural = regex_match.strings[2]
+				plural = _replace_quotes(regex_match.strings[2])
 				context = regex_match.strings[3]
 			msgids_context_plural.push_back([singular, context, plural])
 
 
 func _get_recognized_extensions() -> PackedStringArray:
 	return ["gd"]
+
+
+func _replace_quotes(s: String) -> String:
+	return s.replace(r"\'", "'").replace(r'\"', '"')
